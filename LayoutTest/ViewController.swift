79e4ace78ea0784c23263b91bfa24ff3c80d9f5c
Herman Chan <hermanccw@gmail.com>
2018-02-28 01:14:48 -0500

    Initial Commit

---
+++ LayoutTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutTest
+//
+//  Created by Herman Chan on 2018-02-28.
+//  Copyright © 2018 Herman Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
